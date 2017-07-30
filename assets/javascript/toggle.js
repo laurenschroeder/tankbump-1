11ce07b7f06652a99ab8bd2c80b6b13daa2e2076
faraday <rcheyenne.truss@gmail.com>
2017-07-30 12:10:27 -0400

    cleanup

--- assets/javascript/toggle.js
+++ assets/javascript/toggle.js
@@ -1,6 +1,5 @@
 document.getElementById("toggle").addEventListener("click", function(){
     $("#side-bar").toggleClass("collapsed");
-    //$("#map-container").toggleClass("col-md-12 col-md-9");
     $("#toggle").toggleClass("glyphicon-menu-left");
     $("#toggle").toggleClass("glyphicon-menu-right");
 });
